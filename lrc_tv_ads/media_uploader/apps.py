424d81eef13fad7dd2e77ad7fe170ce4fe65985d
matt <mnbass21@colby.edu>
2021-07-27 00:23:48 -0400

    created media_uploader app

---
+++ lrc_tv_ads/media_uploader/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MediaUploaderConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'media_uploader'
---
+++ lrc_tv_ads/tv_adds/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TvAddsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tv_adds'
