f0281617496d12c8a8cb5b1f21a6097a7519ed65
matt <mnbass21@colby.edu>
2021-07-27 07:03:33 -0400

    change media_uploader landing page

--- lrc_tv_ads/media_uploader/views.py
+++ lrc_tv_ads/media_uploader/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the media_uploader index.")
+    return HttpResponse("This is the media_uploader page.")
 # Create your views here.
