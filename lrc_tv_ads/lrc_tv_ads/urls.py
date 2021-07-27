28ea8b788645a600723d13e5b9124ba961577362
matt <mnbass21@colby.edu>
2021-07-27 00:34:36 -0400

    updated urls to connect media uploader to main lrc_tv_ads

--- lrc_tv_ads/lrc_tv_ads/urls.py
+++ lrc_tv_ads/lrc_tv_ads/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('media_uploader', include('polls.urls')),
+    path('media_uploader', include('media_uploader.urls')),
     path('admin/', admin.site.urls),
 ]
